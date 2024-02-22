a1cd9fbae8e21eb638d4bd18f6537145ee14a937
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-02-22 22:16:00 +0700

    Init project

---
+++ test/serverless-rag-solution.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessRagSolution from '../lib/serverless-rag-solution-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-rag-solution-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessRagSolution.ServerlessRagSolutionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
