ec72885480fe28bb8c1e16e9dfec149b22bb50b2
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-02-24 21:26:41 +0700

    Update VectorDBProvider

---
+++ utils/vector-db-provider.ts
@@ -0,0 +1,9 @@
+export enum VectorDBProvider {
+    Qdrant = "Qdrant",
+    FAISS = "FAISS",
+    ChromaDB = "ChromaDB",
+    LlamaIndex = "LlamaIndex",
+    Weaviate = "Weaviate",
+    Pinecone = "Pinecone",
+    Milvus = "Milvus",
+}
